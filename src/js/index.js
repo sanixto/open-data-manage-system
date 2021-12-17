0fb9e95af365febd0a67ec30d116bada9594a9dd
sanixto <sanix.to@gmail.com>
2021-12-17 20:42:59 +0200

    update index

--- src/js/index.js
+++ src/js/index.js
@@ -22,36 +22,3 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
-
-  // Tests
-  (async () => {
-    let users;
-    // УВАГА: `force: true` спочатку дропне таблицю, а потім створить її знову.
-    try {
-      await User.sync({ force: true });
-      // Тепер таблиця `users` у бд відповідає моделі User
-     
-      await User.create({
-          id: 1,
-          password: '12412',
-          name: 'oleg'
-        });
-
-        users = await User.findAll();
-    } catch {
-      console.log(ERROR, err);
-    }
-    console.log(users);
-  })();
-
-// (async() => {
-//     // Отримати всіх користувачів
-//     let user;
-//     try {
-//       user = await User.findAll();
-//     } catch(err) {
-//       console.log("ERROR 2", err); 
-//       return;
-//     }
-//     console.log("All:", JSON.stringify(users, null, 4));
-// })();
